2d201bccfc16fb56e34454367f92e0ff0f327d5e
jahid-coder <jahidiu@gmail.com>
2023-12-06 14:49:41 +0600

    e_support project

--- routes/web.php
+++ routes/web.php
@@ -45,18 +45,23 @@ Route::post('/password/update', [HomeController::class, 'update_password'])->mid
 
 
 //Role Create
-Route::get('role/index', [RoleController::class, 'index'])->middleware('auth')->name('role.index');
-Route::get('role/create', [RoleController::class,'create'])->middleware('auth')->name('role.create');
-Route::post('role/store', [RoleController::class,'store'])->middleware('auth')->name('role.store');
-Route::get('role/edit/{id}', [RoleController::class,'edit'])->middleware('auth')->name('role.edit');
-Route::post('role/update/{id}', [RoleController::class,'update'])->middleware('auth')->name('role.update');
-Route::get('role/delete/{id}', [RoleController::class,'destroy'])->middleware('auth')->name('role.delete');
+Route::group(['middleware' => 'auth', 'admin'], function(){
+    
+    Route::get('role/index', [RoleController::class, 'index'])->middleware('auth','admin')->name('role.index');
+    Route::get('role/create', [RoleController::class,'create'])->middleware('auth','admin')->name('role.create');
+    Route::post('role/store', [RoleController::class,'store'])->middleware('auth','admin')->name('role.store');
+    Route::get('role/edit/{id}', [RoleController::class,'edit'])->middleware('auth','admin')->name('role.edit');
+    Route::post('role/update/{id}', [RoleController::class,'update'])->middleware('auth','admin')->name('role.update');
+    Route::get('role/delete/{id}', [RoleController::class,'destroy'])->middleware('auth','admin')->name('role.delete');
 
 
-//User Create
-Route::get('user/index', [UserController::class, 'index'])->middleware('auth')->name('user.index');
-Route::get('user/create', [UserController::class,'create'])->middleware('auth')->name('user.create');
-Route::post('user/store', [UserController::class,'store'])->middleware('auth')->name('user.store');
-Route::get('user/edit/{id}', [UserController::class,'edit'])->middleware('auth')->name('user.edit');
-Route::post('user/update/{id}', [UserController::class,'update'])->middleware('auth')->name('user.update');
-Route::get('user/delete/{id}', [UserController::class,'destroy'])->middleware('auth')->name('user.delete');
+    //User Create
+    Route::get('user/index', [UserController::class, 'index'])->middleware('auth','admin')->name('user.index');
+    Route::get('user/create', [UserController::class,'create'])->middleware('auth','admin')->name('user.create');
+    Route::post('user/store', [UserController::class,'store'])->middleware('auth','admin')->name('user.store');
+    Route::get('user/edit/{id}', [UserController::class,'edit'])->middleware('auth','admin')->name('user.edit');
+    Route::post('user/update/{id}', [UserController::class,'update'])->middleware('auth','admin')->name('user.update');
+    Route::get('user/delete/{id}', [UserController::class,'destroy'])->middleware('auth','admin')->name('user.delete');
+
+});
+
