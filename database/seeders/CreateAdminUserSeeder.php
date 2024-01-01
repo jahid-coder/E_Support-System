cd7c1d8cda1221c5ab455f9976b508d03ff16a66
jahidul Islam <jahidiu@gmail.com>
2024-01-01 22:27:08 +0600

    first commet

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -1,34 +1,38 @@
 <?php
-  
+
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
-use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Role
+;
 use Spatie\Permission\Models\Permission;
-  
+
 class CreateAdminUserSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
+        $role = Role::create(['rolename' => 'Admin']);
+
         $user = User::create([
-            'name' => 'Hardik Savani',
-            'userid' => 'ict2010',  
+            'userid' => 'ict2010',
+            'name' => 'Hardik Savani',  
             'email' => 'admin@gmail.com',
             'password' => bcrypt('123456')
         ]);
+    
         
-        $role = Role::create(['rolename' => 'Admin']);
-         
+     
         $permissions = Permission::pluck('id','id')->all();
-       
+   
         $role->syncPermissions($permissions);
-         
+     
         $user->assignRole([$role->id]);
-        //$user->assignRole($adminRole);
     }
 }
--- database/seeders/PermissionTableSeeder.php
+++ database/seeders/PermissionTableSeeder.php
@@ -1,17 +1,19 @@
 <?php
-  
+
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-  
+
 class PermissionTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
         $permissions = [
            'role-list',
@@ -21,11 +23,12 @@ class PermissionTableSeeder extends Seeder
            'product-list',
            'product-create',
            'product-edit',
-           'product-delete',
+           'product-delete'
         ];
-        
+     
         foreach ($permissions as $permission) {
              Permission::create(['name' => $permission]);
         }
     }
+
 }
